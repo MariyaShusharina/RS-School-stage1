d5324945df9f3655eeff65654c413c7eb7078587
Mariya Shusharina <mariya.shushar@gmail.com>
2024-12-02 02:14:29 +0300

    feat: add burger-menu functionality, move resizing events to one function, add comments about structure of code

--- christmas-shop/g-scripts.js
+++ christmas-shop/g-scripts.js
@@ -1,5 +1,18 @@
 
+// Variable for Up-button
+
 const btnUp = document.querySelector(".arrow-up");
+
+// Variables for Burger-menu
+
+const upLine = document.getElementById("up-line");
+const botLine = document.getElementById("bot-line");
+
+const mobMenu = document.querySelector(".mob-menu");
+
+let isMenuOpen = false;
+
+// Up-button functionality
 
 function btnUpDisplay() {
 	if ((window.pageYOffset > 300) && (window.innerWidth < 770)) {
@@ -16,3 +29,59 @@ window.onscroll = function() { btnUpDisplay(); }
 function scrollUp() {
 	window.scrollTo(0, 0);
 }
+
+// Burger-menu functionality
+
+function burger() {
+	if (!isMenuOpen) {
+
+		isMenuOpen = true;
+
+		upLine.style.transform = "translate(0, 4px) rotate(45deg)";
+		botLine.style.transform = "translate(0, -4px) rotate(-45deg)";
+
+		mobMenu.style.transform = "translate(0, 0)";
+
+		document.body.style.overflowY = "hidden";
+
+	} else {
+
+		isMenuOpen = false;
+
+		upLine.style.transform = "translate(0, 0) rotate(0)";
+		botLine.style.transform = "translate(0, 0) rotate(0)";
+
+		mobMenu.style.transform = "translate(100%, 0)";
+
+		document.body.style.overflowY = "auto";
+		
+	}
+}
+
+function hideMenuOnChange() {
+
+	let www = window.innerWidth;
+
+	if (www > 768) {
+
+		document.body.style.overflowY = "auto";
+
+		isMenuOpen = false;
+
+		upLine.style.transform = "translate(0, 0) rotate(0)";
+		botLine.style.transform = "translate(0, 0) rotate(0)";
+
+		mobMenu.style.transform = "translate(100%, 0)";
+	}
+}
+
+
+// Function for all Event Listeners on window
+
+
+function resizer() {
+	hideMenuOnChange();
+}
+
+window.addEventListener("resize", resizer);
+
