9a639e30422f9fd1ee03fa7e2848830c1374f1aa
Mariya Shusharina <mariya.shushar@gmail.com>
2025-01-11 21:26:22 +0300

    feat: add generation of h1 and section, remove body const

--- simon-says/scripts.js
+++ simon-says/scripts.js
@@ -1,6 +1,17 @@
 
-const body = document.body;
+// Heading
+
+let h1 = document.createElement("h1");
+h1.textContent = "Simon Says";
+
+// Section
+
+let section = document.createElement("section");
+
 
 function loadBody() {
-  
+  document.body.appendChild(h1);
+  document.body.appendChild(section);
 }
+
+window.onload = loadBody();
